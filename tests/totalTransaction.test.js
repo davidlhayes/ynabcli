1f6b3f8b549dbf85c50a2f5f71166052db1fbf49
David Hayes <david.hayes@qsrsoft.com>
2024-10-24 19:10:29 -0500

    restore test in package.json

--- tests/totalTransaction.test.js
+++ tests/totalTransaction.test.js
@@ -1,6 +1,6 @@
-  const totalTransaction = require('../lib/totalTransaction');
+  const totalTransactionTest = require('../lib/totalTransaction');
 
-  describe('totalTransaction', () => {
+  describe('totalTransactionTest', () => {
     test('should apply discount correctly', () => {
       const taxedTransaction = [
         { amount: 1000, amounts: [500, 500] },
@@ -10,7 +10,7 @@
       const discount = 0.9;
       const giftCard = null;
 
-      const result = totalTransaction(taxedTransaction, total, discount, giftCard);
+      const result = totalTransactionTest(taxedTransaction, total, discount, giftCard);
 
       expect(result).toEqual([
         { amount: 900, amounts: [450, 450] },
@@ -27,7 +27,7 @@
       const discount = null;
       const giftCard = 300;
 
-      const result = totalTransaction(taxedTransaction, total, discount, giftCard);
+      const result = totalTransactionTest(taxedTransaction, total, discount, giftCard);
 
       expect(result).toEqual([
         { amount: 900, amounts: [450, 450] },
@@ -44,7 +44,7 @@
       const discount = 0.9;
       const giftCard = 300;
 
-      const result = totalTransaction(taxedTransaction, total, discount, giftCard);
+      const result = totalTransactionTest(taxedTransaction, total, discount, giftCard);
 
       expect(result).toEqual([
         { amount: 810, amounts: [405, 405] },
@@ -61,7 +61,7 @@
       const discount = 1;
       const giftCard = 0;
 
-      const result = totalTransaction(taxedTransaction, total, discount, giftCard);
+      const result = totalTransactionTest(taxedTransaction, total, discount, giftCard);
 
       expect(result).toEqual([
         { amount: 1000, amounts: [500, 500] },
